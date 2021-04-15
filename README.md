4c5168a79e3d9416c4928fca71ace2274c46c7d8
gustavohimeno <58175587+gustavohimeno@users.noreply.github.com>
2021-04-15 02:08:57 -0300

    posição da função e update do readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # teste_item9
-Esse repositório conta com a resolução do item 9.
+This repository presents the solution for question 9.
 
 # Descrição Funcional
-O usuário fornece como entrada uma string com máximo de 1000 caracteres e recebe o comprimento da string como resposta.
+The user will type a string with maximum of 1000 characters and he will receive the length of this string as a result.
--- item9.c
+++ item9.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 
-//função para cálculo do comprimento de string
-int calc_length(char *str);
-
-int main()
-    {
-    //declaração de variáveis
-    int lim = 1002;
-    int length;
-    char phrase[lim];
-    
-    //solicita que o usuário digite a string que deseja avaliar no máximo de 1000 caracteres
-    puts("Type a phrase (max = 1000 characteres): ");
-    
-    //recebe a string digitada pelo usuário
-    if (fgets(phrase, sizeof(phrase), stdin) != NULL)
-        {
-        length = calc_length(phrase);
-        printf("%d", length);
-        };
-    
-    return 0;
-    }
-
+//function for length calculation
 int calc_length(char *str)
     {
     int total_length = 0;
@@ -31,4 +9,25 @@ int calc_length(char *str)
         total_length++;
         }
     return total_length - 1;
+    }
+
+
+int main()
+    {
+    //variables declaration
+    int lim = 1002;
+    int length;
+    char phrase[lim];
+    
+    //asks the user to type a string with maximum of 1000 characters
+    puts("Type a phrase (max = 1000 characteres): ");
+    
+    //receives the string typed by the user
+    if (fgets(phrase, sizeof(phrase), stdin) != NULL)
+        {
+        length = calc_length(phrase);
+        printf("%d", length);
+        };
+    
+    return 0;
     }
